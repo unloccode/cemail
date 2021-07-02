2bade94868ca2a01c5a6b89a7f3ac0c0d180068b
unloccode <unloccode@gmail.com>
2021-07-02 22:33:55 +0300

    Major changes

--- src/components/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FontAwesome } from '@fortawesome/react-fontawesome';
-import { faSync } from '@fortawesome/free-solid-svg-icons';
-
-export default props => 
-<div className={`fadeIn ${props.spinning}`}>
-    <FontAwesome icon={faSync} size={props.size} />
-</div>
